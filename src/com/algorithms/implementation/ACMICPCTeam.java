cd3ec24c5baf3cd8d1ab205dbd9ad6dff90fad13
Kuldeep Sidhu <kuldeepsidhu88@gmail.com>
2016-05-16 19:59:13 +0530

    ACMICPCTeam

---
+++ src/com/algorithms/implementation/ACMICPCTeam.java
@@ -0,0 +1,60 @@
+package com.algorithms.implementation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * Link - https://www.hackerrank.com/challenges/acm-icpc-team
+ * 
+ * @author Kuldeep
+ * 
+ */
+public class ACMICPCTeam {
+	public static void main(String[] args) {
+
+		Scanner in = new Scanner(System.in);
+		int noOfStudents = in.nextInt();
+		int noOfTopics = in.nextInt();
+		String topics[] = new String[noOfStudents];
+		for (int topic_i = 0; topic_i < noOfStudents; topic_i++) {
+			topics[topic_i] = in.next();
+		}
+		
+		int max = 0;
+		int number = 0;
+		for (int i = 0; i < noOfStudents; i++) {
+			for (int k = i+1; k < noOfStudents; k++) {
+				char[] stuOne = topics[i].toCharArray();
+				char[] stuTwo = topics[k].toCharArray();
+				char[] result = new char[noOfTopics];
+				for (int j = 0; j < noOfTopics; j++) {
+					if (stuOne[j] == '1' || stuTwo[j] == '1') {
+						result[j] = '1';
+					} else {
+						result[j] = '0';
+					}
+				}
+				//System.out.print("[" + i + "," + k + "]");
+
+				//System.out.println(result);
+				String res = String.valueOf(result);
+				
+				int noOfOccurences = res.length() - res.replace("1", "").length();
+				if(noOfOccurences>max){
+					max=noOfOccurences;
+					number=0;
+				}
+				if(noOfOccurences==max){
+					number++;
+				}
+			}
+		}
+		
+		System.out.println(max);
+		System.out.println(number);
+
+		in.close();
+	}
+	
+}
