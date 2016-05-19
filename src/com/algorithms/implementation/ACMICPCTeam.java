89763708d91076294740fc5e5d2b3fab6bb7da3c
kuldeepsidhu88 <kuldeepsidhu88@gmail.com>
2016-05-19 20:02:02 +0530

    ACMICPCTeam

--- src/com/algorithms/implementation/ACMICPCTeam.java
+++ src/com/algorithms/implementation/ACMICPCTeam.java
@@ -18,47 +18,46 @@ public class ACMICPCTeam {
 		for (int topic_i = 0; topic_i < noOfStudents; topic_i++) {
 			topics[topic_i] = in.next();
 		}
-		
+
 		int max = 0;
 		int number = 0;
 		for (int i = 0; i < noOfStudents; i++) {
-			for (int k = i+1; k < noOfStudents; k++) {
-				//char[] stuOne = topics[i].toCharArray(); int[] a
-				//char[] stuTwo = topics[k].toCharArray(); int[] b
-				char[] result = new char[noOfTopics];
-				
+			for (int k = i + 1; k < noOfStudents; k++) {
+
+				int[] stuOneInt = stringToIntArray(topics[i]);
+				int[] stuTwoInt = stringToIntArray(topics[k]);
+
+				int noOfOccurences = 0;
+
 				for (int j = 0; j < noOfTopics; j++) {
-					/*if (stuOne[j] == '1' || stuTwo[j] == '1') {
-						result[j] = '1';
-					} else {
-						result[j] = '0';
-					}
-					*/
-					
-					if (topics[i].charAt(j) == '1' || topics[k].charAt(j) == '1') { // a[i]|b[i]
-						result[j] = '1';
-					} else {
-						result[j] = '0';
-					}
+					int num = stuOneInt[j] | stuTwoInt[j];
+					if (num == 1)
+						noOfOccurences++;
 				}
 
-				String res = String.valueOf(result);
-				
-				int noOfOccurences = res.length() - res.replace("1", "").length();
-				if(noOfOccurences>max){
-					max=noOfOccurences;
-					number=0;
+				if (noOfOccurences > max) {
+					max = noOfOccurences;
+					number = 0;
 				}
-				if(noOfOccurences==max){
+				if (noOfOccurences == max) {
 					number++;
 				}
 			}
 		}
-		
+
 		System.out.println(max);
 		System.out.println(number);
 
 		in.close();
 	}
-	
+
+	public static int[] stringToIntArray(String number) {
+		int length = number.length();
+		int[] result = new int[length];
+		for (int i = 0; i < length; i++) {
+			result[i] = Character.getNumericValue(number.charAt(i));
+		}
+		return result;
+	}
+
 }
